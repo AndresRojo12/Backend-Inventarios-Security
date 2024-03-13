88a38bf6b51641d9d2c535564ac2c271efdfeca5
AndresRojo12 <andresrojo20013@gmail.com>
2024-03-13 13:19:30 -0500

    se agregan console en el store para verifcar guardado del token

--- frontend/src/store/auth.js
+++ frontend/src/store/auth.js
@@ -1,5 +1,4 @@
 import { defineStore } from "pinia";
-
 const useAuth = defineStore("auth", {
   state: () => {
     return {
@@ -9,40 +8,39 @@ const useAuth = defineStore("auth", {
       baseURL: "http://localhost:3001/api",
     };
   },
-
+  
   actions: {
     async Login(email, password) {
       const uri = `${this.baseURL}/logins`;
-
+      
       try {
         const respo = await fetch(uri, {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
-            "Accept": "Application/json",
+            Accept: "Application/json",
           },
           body: JSON.stringify({
-            'email': email,
-            'password': password,
+            email: email,
+            password: password,
           }),
         });
-
+        
         const res = await respo.json();
-
+        
         console.log("Respuesta del backend:", res);
-
+        
         if (res.acces_token && res.acces_token.trim() !== "") {
           this.acces_token = res.acces_token.trim();
-          this.user = res.user; // Almacena la información del usuario
-          console.log("Token almacenado en el frontend:", this.acces_token);
-          localStorage.setItem('acces_token', this.acces_token);
-
+          
           this.user = {
-            _id: res._id,
+            _id: res._id || null,
             nombre: res.nombre,
             rol: res.rol,
             email: res.email,
           };
+          console.log("Token almacenado en el frontend:", this.acces_token);
+          localStorage.setItem("acces_token", this.acces_token);
           return true;
         } else {
           console.error("Token inválido o vacío.");
@@ -53,36 +51,39 @@ const useAuth = defineStore("auth", {
         return false;
       }
     },
-
+    
     async Register(nombre, email, password, rol) {
       if (!this.acces_token) {
         console.error("Usuario no autorizado. Inicia sesión primero.");
         return false;
       }
-
-      console.log("Token enviado desde el frontend para el registro:", this.acces_token);
-
-      const uri = `${this.baseURL}/usuarios`
-
-      try {
-        const respo = await fetch(uri, {
-          method: 'POST',
+      
+      console.log(
+        "Token enviado desde el frontend para el registro:",
+        this.acces_token
+        );
+        
+        const uri = `${this.baseURL}/usuarios`;
+        
+        try {
+          const respo = await fetch(uri, {
+            method: "POST",
           headers: {
             "Content-Type": "application/json",
-            "Accept": "Application/json",
-            "Authorization": this.acces_token
+            Accept: "Application/json",
+            Authorization: this.acces_token,
           },
           body: JSON.stringify({
-            'nombre': nombre,
-            'email': email,
-            'password': password,
-            'rol': rol
-          })
+            nombre: nombre,
+            email: email,
+            password: password,
+            rol: rol,
+          }),
         });
-
+        
         const res = await respo.json();
         console.log("Respuesta del backend:", res);
-
+        
         if (!res) {
           console.error("Error en la solicitud de registro: Respuesta vacía");
           return false;
@@ -100,62 +101,226 @@ const useAuth = defineStore("auth", {
         console.error("Usuario no autorizado. Inicia sesión primero.");
         return false;
       }
-
-      
-      const uri = `${this.baseURL}/usuarios`
-
+      const uri = `${this.baseURL}/usuarios`;
       try {
         const respo = await fetch(uri, {
-          method: 'GET',
+          method: "GET",
           headers: {
             "Content-Type": "application/json",
-            "Accept": "Application/json",
-            "Authorization": this.acces_token
-          }
-          
+            Accept: "Application/json",
+            Authorization: this.acces_token,
+          },
         });
-
+        
         const users = await respo.json();
         console.log("Respuesta del backend:", users);
-
-        return users
+        
+        return users;
+      } catch (error) {
+        console.error("Error en la solicitud de get:", error);
+        return null;
+      }
+    },
+    
+    async getMarca() {
+      if (!this.acces_token) {
+        console.error("Usuario no autorizado. Inicia sesión primero.");
+        return false;
+      }
+      const uri = `${this.baseURL}/marcas/?estado=true`;
+      try {
+        const respo = await fetch(uri, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "Application/json",
+            Authorization: this.acces_token,
+          },
+        });
+        
+        const marcas = await respo.json();
+        console.log("Respuesta del backend:", marcas);
+        
+        return marcas;
       } catch (error) {
         console.error("Error en la solicitud de get:", error);
         return null;
       }
     },
 
+    async getEstado() {
+      if (!this.acces_token) {
+        console.error("Usuario no autorizado. Inicia sesión primero.");
+        return false;
+      }
+      const uri = `${this.baseURL}/estadosequipos?estado=true`;
+      try {
+        const respo = await fetch(uri, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "Application/json",
+            Authorization: this.acces_token,
+          },
+        });
+        
+        const estados = await respo.json();
+        console.log("Respuesta del backend:", estados);
+        
+        return estados;
+      } catch (error) {
+        console.error("Error en la solicitud de get:", error);
+        return null;
+      }
+    },
+    
     async RegisterEquipo(nombre) {
       if (!this.acces_token) {
         console.error("Usuario no autorizado. Inicia sesión primero.");
         return false;
       }
-
-      console.log("Token enviado desde el frontend para el registro:", this.acces_token);
-
-      const uri = `${this.baseURL}/tiposequipos`
-
-      try {
-        const respo = await fetch(uri, {
-          method: 'POST',
+      
+      console.log(
+        "Token enviado desde el frontend para el registro:",
+        this.acces_token
+        );
+        
+        const uri = `${this.baseURL}/tiposequipos`;
+        
+        try {
+          const respo = await fetch(uri, {
+            method: "POST",
           headers: {
             "Content-Type": "application/json",
-            "Accept": "Application/json",
-            "Authorization": this.acces_token
+            Accept: "Application/json",
+            Authorization: this.acces_token,
           },
           body: JSON.stringify({
-            'nombre': nombre
-          })
+            nombre: nombre,
+          }),
         });
-
+        
         if (!respo.ok) {
-          console.error("Error en la solicitud de registro. Código de estado:", respo.status);
+          console.error(
+            "Error en la solicitud de registro. Código de estado:",
+            respo.status
+            );
+            return false;
+          }
+          
+          const res = await respo.json();
+          console.log("Respuesta del backend:", res);
+          
+          if (!res) {
+            console.error("Error en la solicitud de registro: Respuesta vacía");
+            return false;
+          } else {
+            return true;
+          }
+        } catch (error) {
+          console.error("Error en la solicitud de registro:", error);
           return false;
         }
+      },
+      
+      async getTipoEquipo() {
+        if (!this.acces_token) {
+        console.error("Usuario no autorizado. Inicia sesión primero.");
+        return false;
+      }
+      const uri = `${this.baseURL}/tiposequipos?estado=true`;
+      
+      try {
+        const respo = await fetch(uri, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "Application/json",
+            Authorization: this.acces_token,
+          },
+        });
+        
+        const tEquipos = await respo.json();
+        console.log("Respuesta del backend:", tEquipos);
+        
+        return tEquipos;
+      } catch (error) {
+        console.error("Error en la solicitud de get:", error);
+        return null;
+      }
+    },
+    
+    async RegisterInventario(serial, Modelo, Descripción, FotoEquipo, Color, FechaCompra, precio, usuario, marca, estado, tipoEquipo) {
+      if (!this.acces_token) {
+        console.error("Usuario no autorizado. Inicia sesión primero.");
+        return false;
+      }
+      
+      console.log("Token enviado desde el frontend para el registro:", this.acces_token);
+      console.log('Usuario para el registro:', usuario);
+    
+      
+      const fechaCompra = new Date(FechaCompra).toISOString();
+    
+      
+      const precioNum = parseFloat(precio);
+      if (isNaN(precioNum)) {
+        console.error("Precio no válido");
+        return false;
+      }
 
+     
+    
+      console.log('Datos enviados al backend:', {
+        serial,
+        Modelo,
+        Descripción,
+        FotoEquipo,
+        Color,
+        FechaCompra: fechaCompra,
+        precio: precioNum,
+        usuario,
+        marca,
+        estado,
+        tipoEquipo,
+      });
+    
+      const uri = `${this.baseURL}/inventarios`;
+    
+      try {
+        const respo = await fetch(uri, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "Application/json",
+            Authorization: this.acces_token,
+          },
+          body: JSON.stringify({
+            serial,
+            Modelo,
+            Descripción,
+            FotoEquipo,
+            Color,
+            FechaCompra: fechaCompra,
+            precio: precioNum,
+            usuario,
+            marca,
+            estado,
+            tipoEquipo
+          }),
+        });
+    
+        if (!respo.ok) {
+          console.error("Error de red o de servidor:", respo.status, respo.statusText);
+          
+          const errorDetails = await respo.json();  
+          console.error("Detalles del error:", errorDetails);
+          return false;
+        }
+    
         const res = await respo.json();
         console.log("Respuesta del backend:", res);
-
+    
         if (!res) {
           console.error("Error en la solicitud de registro: Respuesta vacía");
           return false;
@@ -168,56 +333,29 @@ const useAuth = defineStore("auth", {
       }
     },
     
-    async getTipoEquipo() {
-      if (!this.acces_token) {
-        console.error("Usuario no autorizado. Inicia sesión primero.");
-        return false;
-      }
-      const uri = `${this.baseURL}/tiposequipos?estado=true`
 
-      try {
-        const respo = await fetch(uri, {
-          method: 'GET',
-          headers: {
-            "Content-Type": "application/json",
-            "Accept": "Application/json",
-            "Authorization": this.acces_token
-          }
-          
-        });
-
-        const tEquipos = await respo.json();
-        console.log("Respuesta del backend:", tEquipos);
-
-        return tEquipos
-      } catch (error) {
-        console.error("Error en la solicitud de get:", error);
-        return null;
-      }
-    },
-
+    
     async getInventarios() {
       if (!this.acces_token) {
         console.error("Usuario no autorizado. Inicia sesión primero.");
         return false;
       }
-      const uri = `${this.baseURL}/inventarios?estado=false`
+      const uri = `${this.baseURL}/inventarios?estado=false`;
 
       try {
         const respo = await fetch(uri, {
-          method: 'GET',
+          method: "GET",
           headers: {
             "Content-Type": "application/json",
-            "Accept": "Application/json",
-            "Authorization": this.acces_token
-          }
-          
+            Accept: "Application/json",
+            Authorization: this.acces_token,
+          },
         });
 
         const tEquipos = await respo.json();
         console.log("Respuesta del backend:", tEquipos);
 
-        return tEquipos
+        return tEquipos;
       } catch (error) {
         console.error("Error en la solicitud de get:", error);
         return null;
@@ -226,7 +364,7 @@ const useAuth = defineStore("auth", {
 
     logout() {
       this.acces_token = null;
-      this.user = null; 
+      this.user = null;
     },
   },
 });
