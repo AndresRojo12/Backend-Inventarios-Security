2115ff52b677ea0f97efe111ccff02cf43eb6ba2
AndresRojo12 <andresrojo20013@gmail.com>
2024-05-18 10:51:20 -0500

    mongo

--- databas/configuration.js
+++ databas/configuration.js
@@ -3,10 +3,7 @@ const mongoose=require('mongoose')
 const conection= async()=>{
 
     try {
-        await mongoose.connect(process.env.MONGO_URI,{
-            useNewUrlParser: true, 
-            useUnifiedTopology: true
-        })
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("Conexion correcta a Mongo")
     } catch (error) {
         console.log('Error',error)
